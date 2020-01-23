f0ce83b581fc07fc41f1f9118c47eba141fb8c6c
User <c2h8e9d4@gmail.com>
2020-01-23 20:54:15 +0300

    добавил переменные в script.js

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const money = 100;
+const income = 'hellow';
+const addExpenses = 200;
+const deposit = 5000;
+const mission = 'JS';
+const period = [10, 20, 30];
+
+let message = 'Привет';
+
+alert('привет');
