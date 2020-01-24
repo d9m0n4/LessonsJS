3fd0c340c01953b0f8a4385e87e50c437e7eea3e
User <c2h8e9d4@gmail.com>
2020-01-24 19:46:38 +0300

    изменил значения переменных в script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,8 @@
-
-
-const money = 100;
-const income = 'hellow';
-const addExpenses = 200;
-const deposit = 5000;
-const mission = 'JS';
-const period = [10, 20, 30];
-
-let message = 'Привет';
-
-alert(message);
+let money,
+    income, 
+    addExpenses,
+    deposit,
+    mission,
+    period;
+alert('привет Андрей');
+console.log('Доброго времени суток товарищи!!!');
