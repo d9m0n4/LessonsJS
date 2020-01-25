ee587b04c9d0157471a997a90fe79ad0bad93cc7
user <c2h8e9d4@gmail.com>
2020-01-25 20:55:09 +0300

    изменил файл script для втрого дз

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
-let money,
-    income, 
-    addExpenses,
-    deposit,
-    mission,
-    period;
-alert('привет Андрей');
-console.log('Доброго времени суток товарищи!!!');
+let money = 50000,
+  income = 'Фриланс',
+  addExpenses = 'Интернет, Коммуналка, Такси',
+  deposit = true,
+  mission = 30000000000,
+  period = 6,
+  budgetDay = money / 30;
+
+addExpenses.toUpperCase();
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(' '));
+console.log(budgetDay);
