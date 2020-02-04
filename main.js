9c49ad5ae800dcd1fee0e433467bd3e063f995ab
user <c2h8e9d4@gmail.com>
2020-02-04 18:18:26 +0300

    ДЗ 9 Урок =)

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict'
 
-let start = document.getElementById('start'),
+let startBtn = document.getElementById('start'),
     plusIncom = document.getElementsByClassName('income_add'),
     plusExpenses = document.getElementsByClassName('expenses_add'),
     depositCheck = document.querySelector('#deposit-check'),
     addIncomeItem = document.querySelectorAll('.additional_income-item'),
-
     budgetMonth = document.getElementsByClassName('budget_month-value'),
     budgetDay = document.getElementsByClassName('budget_day-value'),
     expensesMonth = document.getElementsByClassName('expenses_month-value'),
@@ -13,13 +12,11 @@ let start = document.getElementById('start'),
     additionalExpenses = document.getElementsByClassName('additional_expenses-value'),
     incomePeriod = document.getElementsByClassName('income_period-value'),
     targetMonth = document.getElementsByClassName('target_month-value'),
-
     expensesTitle = document.querySelector('.expenses-title'),
     expensesAmount = document.querySelector('.expenses-amount'),
-
     addExpensesItem = document.querySelector('.additional_expenses-item'),
-
     targetAmount = document.querySelector('.target-amount'),
-
     periodSelect = document.querySelector('.period-select'),
     periodAmount = document.querySelector('.period-amount');
+
+    
