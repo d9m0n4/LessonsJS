a9c9e2d9acd2dd7e0286fa015fd1ee41c8dfde50
User <c2h8e9d4@gmail.com>
2020-02-12 13:17:38 +0300

    дз 14.3

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
+document.addEventListener('DOMContentLoaded', function(){
+  'use strict';
 
-'use strict';
+  const DomElement = function ({height, width, bg}) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+  };
+  console.dir(DomElement);
 
-const DomElement = function ({selector, height, width, bg, fontSize, text}) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-  this.text = text;
-};
-console.dir(DomElement);
-
-DomElement.prototype.createElement = function(){
-  let tagName = this.selector.slice(0) === '.' ? 'div' : 'p',
-      createdElem = document.createElement(tagName);
-
-      if (this.selector.slice(0) === '.') {
-        tagName = 'div';
-        createdElem.className = this.selector.slice(1);
-      } else {
-        tagName = 'p';
-        createdElem.id = this.selector.slice(1);
-      }
-
-      createdElem.style.cssText = 'height:' + this.height + ';' +
-                                  'width:' + this.width + ';' +
-                                  'color:' + this.bg + ';' +
-                                  'font-size:' + this.fontSize;                     
-      createdElem.innerText = this.text;
   
-
-  document.querySelector('body').appendChild(createdElem);
-};
-
-
-
-
-
-
-
-let newDom = new DomElement({selector: '.block', height: '200px', width: '250px', bg: 'red', fontSize: '16px', text: 'asdsdasd' });
-let newDom1 = new DomElement({selector: '#p', height: '200px', width: '250px', bg: 'orange', fontSize: '16px', text: 'asdasdasdasdasdasd' });
+  DomElement.prototype.eventListener = function(){
+    document.querySelector('body').addEventListener('keydown', function(event){
+        let element = document.querySelector('.square');
+          if (event.keyCode === 39) {
+            element.style.left = parseInt(getComputedStyle(element).left) + 10 + 'px'
+          }
+          if (event.keyCode === 37) {
+            element.style.left = parseInt(getComputedStyle(element).left) - 10 + 'px'
+          }
+          if (event.keyCode === 38) {
+            element.style.top = parseInt(getComputedStyle(element).top) - 10 + 'px'
+          }
+          if (event.keyCode === 40) {
+            element.style.top = parseInt(getComputedStyle(element).top) + 10 + 'px'
+          }
+      })
+  };
   
-newDom.createElement();
-newDom1.createElement();
+  DomElement.prototype.createElement = function(){
+    let square = document.createElement('div');
+    square.className = 'square';
+
+    square.style.cssText = 'height:' + this.height + ';' +
+                            'width:' + this.width + ';' +
+                            'background-color:' + this.bg + ';' +
+                            'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);';
+                            
+                            
+                                                         
+    document.querySelector('body').appendChild(square);
+  };
+  
+  let newDom = new DomElement({height: '200px', width: '200px', bg: 'red'});
+  
+  newDom.createElement();
+  newDom.eventListener();
+  console.log(newDom);
+  
+})
+
+
