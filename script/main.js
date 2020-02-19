603ee2897e823a7f45e10aa899c371c4a99c5fa9
User <c2h8e9d4@gmail.com>
2020-02-19 13:23:02 +0300

    Задание 17 урока усложненное

--- script/main.js
+++ script/main.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
   'use strict';
-
-  function countTimer(deadline) {
+  let date;
+  date = new Date();
+  date.setDate(date.getDate() + 1);
+  date.setHours(0, 0, 0);
+  function countTimer() {
     let timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
 
     function getTimeRemaining(){
-      let dateStop = new Date(deadline).getTime(),
+      let dateStop = date.getTime(),
           dateNow = new Date().getTime(),
           timeRemaining = (dateStop - dateNow) / 1000,
           seconds = Math.floor(timeRemaining % 60) < 10 ? '0' + Math.floor(timeRemaining % 60)
@@ -25,22 +28,14 @@ document.addEventListener('DOMContentLoaded', function(){
       timerHours.textContent = timer.hours;
       timerMinutes.textContent = timer.minutes;
       timerSeconds.textContent = timer.seconds;
-      if (timer.timeRemaining < 0) {
-        clearInterval(intervalTime);
-          let a = new Date();
-          a.setHours(24,0,0,0);
-        countTimer(a);
+      if (timer.timeRemaining < 1) {
+        date.setDate(date.getDate() + 1);
       }
     }
-    let intervalTime = setInterval(updateClock, 1000);
-
-    
-    
+    setInterval(updateClock, 1000);
     updateClock();  
     
   }
-let a = new Date();
-a.setHours(24,0,0,0);
-countTimer(a);
+countTimer();
 });
 
