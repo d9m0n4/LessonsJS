3b52ee7e89e6e1dfb76ee2c503955558634c9518
User <c2h8e9d4@gmail.com>
2020-02-20 15:21:41 +0300

    Плавная прокрутка при нажатии на кнопку

--- script/main.js
+++ script/main.js
@@ -121,6 +121,15 @@ const btnMenu = document.querySelector('.menu'),
 
   togglePopup();
 
+  // Scroll Button // 
+  const startServicesBtn = document.querySelector('a[href="#service-block"]'),
+        serviceBlock = document.querySelector(`#service-block`);
+  
+  
+    startServicesBtn.addEventListener('click', (e) => {
+      e.preventDefault()
+      serviceBlock.scrollIntoView({block: 'start', behavior: 'smooth'});
+    })
 });
 
 
