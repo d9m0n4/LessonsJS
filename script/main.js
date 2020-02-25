f8c32363b294783b077d63f6d3105aea2eaa7c9e
user <c2h8e9d4@gmail.com>
2020-02-26 00:17:53 +0300

    задание 22 урока калькулятор на сайте + усложенный

--- script/main.js
+++ script/main.js
@@ -350,6 +350,83 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
     validform();
+
+    //calc//
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            clacSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+        let count = 0;
+
+        const countSum = () => {
+            let total = 0,
+                time,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +clacSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = Math.ceil(price * typeValue * squareValue * countValue * dayValue);
+            }
+
+            // let anim;
+            // const animateTotal = () => {
+            //     anim = requestAnimationFrame(animateTotal);
+            //     if (count < total) {
+            //         count += 1;
+            //         totalValue.textContent = count;
+            //         console.log(total);
+            //     } else if (count > total) {
+            //         count -= 1;
+            //         totalValue.textContent = count;
+            //     } else if (count === total) {
+            //         cancelAnimationFrame(anim);
+            //     }
+            // };
+            // animateTotal();
+
+            const animTotal = () => {
+                const interval = setInterval(() => {
+                    time = (1000 / count);
+                    if (count < total) {
+                        count += 1;
+                        totalValue.textContent = count;
+                    } else if (count > total) {
+                        count -= 1;
+                        totalValue.textContent = count;
+                    } else if (count === total) {
+                        clearInterval(interval);
+                    }
+                }, time);
+            };
+            animTotal();
+            console.log(total);
+            
+        };
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+
+            if (target.matches('.calc-type') || target.matches('.calc-square') ||
+             target.matches('.calc-day') || target.matches('.calc-count')) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
 });
 
 
